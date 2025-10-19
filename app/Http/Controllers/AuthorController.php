44fd59d24103e1d812139c6ec0d02fb972905e82
JEKII <achmadzaki2804@gmail.com>
2025-10-19 17:26:48 +0700

    tugas 5 laravel

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -57,4 +57,88 @@ class AuthorController extends Controller
             'data' => $author
         ], 201);
     }
+
+         public function show(string $id)
+{
+    $author = Author::find($id);
+
+    if (!$author) {
+        // jika data tidak ditemukan
+        return response()->json([
+            'success' => false,
+            'message' => 'Resource not found'
+        ], 404); 
+    }
+
+    // jika data ditemukan
+    return response()->json([
+        'success' => true,
+        'message' => 'Get detail resource',
+        'data' => $author
+    ], 200);
+}
+
+public function destroy(string $id)
+{
+    $author = Author::find($id);
+
+    if (!$author) {
+        return response()->json([
+            'success' => false,
+            'message' => 'Resource not found'
+        ], 404);
+    }
+
+    $author->delete();
+
+    return response()->json([
+        'success' => true,
+        'message' => 'Delete resource successfully'
+    ], 200);
+}
+
+public function update(Request $request, string $id)
+{
+    // 1. cari data author berdasarkan id
+    $author = Author::find($id);
+
+    if (!$author) {
+        return response()->json([
+            'success' => false,
+            'message' => 'Resource not found'
+        ], 404);
+    }
+
+    // 2. validasi input
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|string|max:100',
+        'birth_year' => 'required|integer|min:1000|max:' . date('Y'),
+        'nationality' => 'required|string|max:100'
+    ]);
+
+    if ($validator->fails()) {
+        return response()->json([
+            'success' => false,
+            'message' => $validator->errors()
+        ], 422);
+    }
+
+    // 3. siapkan data untuk diupdate
+    $data = [
+        'name' => $request->name,
+        'birth_year' => $request->birth_year,
+        'nationality' => $request->nationality,
+    ];
+
+    // 4. update data ke database
+    $author->update($data);
+
+    // 5. kirim response sukses
+    return response()->json([
+        'success' => true,
+        'message' => 'Resource updated successfully!',
+        'data' => $author
+    ], 200);
+}
+
 }
--- app/Http/Controllers/GenreController.php
+++ app/Http/Controllers/GenreController.php
@@ -55,4 +55,87 @@ class GenreController extends Controller
             'data' => $genre
         ], 201);
     }
+
+     public function show(string $id)
+{
+    $genre = Genre::find($id);
+
+    if (!$genre) {
+        // jika data tidak ditemukan
+        return response()->json([
+            'success' => false,
+            'message' => 'Resource not found'
+        ], 404); 
+    }
+
+    // jika data ditemukan
+    return response()->json([
+        'success' => true,
+        'message' => 'Get detail resource',
+        'data' => $genre
+    ], 200);
+}
+
+public function destroy(string $id)
+{
+    $genre = Genre::find($id);
+
+    if (!$genre) {
+        return response()->json([
+            'success' => false,
+            'message' => 'Resource not found'
+        ], 404);
+    }
+
+    $genre->delete();
+
+    return response()->json([
+        'success' => true,
+        'message' => 'Delete resource successfully'
+    ], 200);
+}
+
+public function update(Request $request, string $id)
+{
+    // 1. cari data genre berdasarkan id
+    $genre = Genre::find($id);
+
+    if (!$genre) {
+        return response()->json([
+            'success' => false,
+            'message' => 'Resource not found'
+        ], 404);
+    }
+
+    // 2. validasi input
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|string|max:100',
+        'description' => 'required|string',
+    ]);
+
+    if ($validator->fails()) {
+        return response()->json([
+            'success' => false,
+            'message' => $validator->errors()
+        ], 422);
+    }
+
+    // 3. siapkan data untuk diupdate
+    $data = [
+        'name' => $request->name,
+        'description' => $request->description,
+    ];
+
+    // 4. update data ke database
+    $genre->update($data);
+
+    // 5. kirim response
+    return response()->json([
+        'success' => true,
+        'message' => 'Resource updated successfully!',
+        'data' => $genre
+    ], 200);
+}
+
+
 }
